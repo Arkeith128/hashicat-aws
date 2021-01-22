95345ba86d6bd91fc6274adf30f78f8b0c918d57
Arkeith <arkeith.cunningham@gmail.com>
2021-01-22 22:17:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/Arkeith-Training/s3-bucket/aws"
   version = "1.15.0"
-  bucket_prefix = "Arkeith"
+  bucket_prefix = "fnmaarkeith"
   # insert required variables here
 }
